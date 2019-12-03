e7bd01af61145ff4bfd01e7fc56048e0093c5ed5
Artur Dzialo <arturdzialo@gmail.com>
2019-12-03 21:17:51 +0100

    One more test

---
+++ ExpressionTests/ComplexOperations/AllOperationsTests.cs
@@ -0,0 +1,25 @@
+﻿using Mathema.Algorithms.Handlers;
+using Mathema.Algorithms.Parsers;
+using NUnit.Framework;
+
+namespace ExpressionTests.ComplexOperations
+{
+    [TestFixture]
+    public class AllOperationsTests
+    {
+        [Test]
+        public void AllOperators()
+        {
+            //Arrange
+            var text = "(4- 6 * -2 )^1/(2 + 2)^(1/2)";/// ;
+            var expected = 8;
+
+            //Act
+            var rpn = RPNParser.Parse(text);
+            var actual = ExpressionBuilder.Build(rpn).Value();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
